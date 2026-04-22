a4a0117ddce3793541ddb2df01b626596ae809ac
Levy Nkeneng <levynkeneng@Levys-MacBook-Air.local>
2026-04-22 15:20:52 +0200

    Minimal Nix config

---
+++ flake.nix
@@ -0,0 +1,103 @@
+{
+  description = "LinkNexus nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    eza = {
+      url = "github:eza-community/eza";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, eza, rust-overlay }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [ 
+	        pkgs.neovim
+	        pkgs.tmux		
+	        pkgs.ghostty-bin
+		pkgs.fzf
+		pkgs.zoxide
+		pkgs.starship
+		pkgs.stow
+      ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      fonts.packages = [
+      	pkgs.nerd-fonts.monaspace
+      ];
+
+	    homebrew = {
+	      enable = true;
+	      casks = [
+	        "raycast"
+	        "zen-browser"
+	        "thunderbird"
+	        "visual-studio-code@insiders"
+	      ];
+	      onActivation.cleanup = "zap";
+	      onActivation.autoUpdate = true;
+	      onActivation.upgrade = true;
+	    };
+
+	    system.primaryUser = "levynkeneng";
+
+	    system.defaults = {
+	      dock.autohide = true;
+	      dock.persistent-apps = [
+	        "${pkgs.ghostty-bin}/Applications/Ghostty.app"
+	        "/Applications/Thunderbird.app"
+	        "/Applications/Zen.app"
+	      ];
+	    };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."main" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+      	configuration
+	      nix-homebrew.darwinModules.nix-homebrew {
+	        nix-homebrew = {
+	          enable = true;
+	          enableRosetta = true;
+	          user = "levynkeneng";
+	        };
+	      }
+      ];
+    };
+  };
+}
